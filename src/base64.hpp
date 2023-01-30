fc946746de12b82a5ef2ed099a7316be5a4f7b7f
Kazuki Ota <ota_droid@live.jp>
2023-01-30 17:15:41 +0900

    fix ns declare.

--- src/base64.hpp
+++ src/base64.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
-void BASE64::encode(const uint8_t* input, size_t inputLength, char* output);
-size_t BASE64::encodeLength(size_t inputLength);
-void BASE64::decode(const char* input, uint8_t* output);
-size_t BASE64::decodeLength(const char* input);
+namespace BASE64{
+    void encode(const uint8_t* input, size_t inputLength, char* output);
+    size_t encodeLength(size_t inputLength);
+    void decode(const char* input, uint8_t* output);
+    size_t decodeLength(const char* input);
+}
