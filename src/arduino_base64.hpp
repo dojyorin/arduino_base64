cf1ac1a9ab1c831bd9bcc6e9437d5cca15cc90b6
Kazuki Ota <ota_droid@live.jp>
2023-05-11 03:19:43 +0900

    fix version.

--- src/arduino_base64.hpp
+++ src/arduino_base64.hpp
@@ -4,12 +4,12 @@
 #include "string.h"
 
 /**
-* Convert between binary and Base64.
+* Convert between binary and Base64 encoded string.
 * @see https://github.com/dojyorin/arduino_base64
 */
 namespace base64{
     /**
-    * Convert binary to Base64.
+    * Convert binary to Base64 encoded string.
     * If the input is string, cast it to `uint8_t*`.
     * @param input Binary data.
     * @param inputLength Number of input bytes.
@@ -25,7 +25,7 @@ namespace base64{
     size_t encodeLength(size_t inputLength);
 
     /**
-    * Convert Base64 to binary.
+    * Convert Base64 encoded string to binary.
     * If the output is string, cast it to `char*`.
     * @param input Base64 encoded string.
     * @param output Binary data.
